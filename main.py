ed61f463cb41f533558a94d4a4bb64849c71485b
Balkrishna Oli <balkrishna.oli@veribom.com>
2024-07-22 14:53:44 +0545

    added chrome-options-to-run-headless

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+chrome_options = Options()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 url = 'https://www.javatpoint.com/devops'
 
 driver.get(url)
